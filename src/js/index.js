b145be80eca896850c21f811c7f91622ff3d026f
boumlik-youtub <mohamedboumlik000@gmail.com>
2022-07-05 19:51:24 +0100

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import '@laylazi/bootstrap-rtl/dist/js/bootstrap.min.js';
+import 'jquery/dist/jquery.min.js';
+import 'popper.js/dist/popper.min';
+import '../sass/style.scss';
+import '@fortawesome/fontawesome-free/js/all.min.js'; 
+
+console.log("Hello, Webpack!");
