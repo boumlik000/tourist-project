907a4b2bcace42d092ae474e2b80cf2a3f711db6
boumlik-youtub <mohamedboumlik000@gmail.com>
2022-07-08 18:14:29 +0100

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,20 @@ import 'popper.js/dist/popper.min';
 import '../sass/style.scss';
 import '@fortawesome/fontawesome-free/js/all.min.js'; 
 
-console.log("Hello, Webpack!");
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
